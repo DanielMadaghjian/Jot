3b9ddc092ce79b5389b682e1a9f0e2c3e52f1aa2
Daniel Madaghjian <danielmadaghjian@gmail.com>
2021-09-19 21:49:25 +0100

    test-2

--- resources/js/views/ContactsShow.vue
+++ resources/js/views/ContactsShow.vue
@@ -11,7 +11,7 @@
                             class = "px-4 py-2 rounded text-green-500 border border-green-500 text-sm font-bold mr-2">Edit</router-link> 
                             <a href="#" class= "px-4 py-2 border border-red-500 rounded text-sm font-bold text-red-500" @click="modal = ! modal">Delete</a>
                             <div v-if="modal" class = "absolute bg-blue-900 text-white rounded-lg z-20 p-8 w-64 right-0 mt-2 mr-6">
-                                <p>Are you sure you want to delete this record?</p>
+                                <p>Are you very sure you want to delete this record?</p>
                                 <div class = "flex items-center mt-6 justify-end">
                                     <button class = "text-white pr-4 " @click="modal = ! modal">Cancel</button>
                                     <button class = "px-4 py-2 bg-red-500 rounded text-sm font-bold text-white " @click="destroy">Delete</button>
@@ -22,8 +22,33 @@
                     <div v-if = "modal" class = "bg-black opacity-25 absolute right-0 left-0 top-0 bottom-0 z-10" @click="modal = ! modal"></div>
                     </div>
 
+                    <div class = "flex flex-col items-center ">
+                        <UserCircle :name = "contact.name"/>
+                        <p class = "pl-5 text-xl font-bold pb-4">{{contact.name}}</p>
+                    </div>
+                    <div class = "px-60 ">
+                        <div class = "border-gray-700 rounded-xl border-2 text-left p-5 bg-gray-200">
+                            <div class = "flex flex-row">
+                                <div>
+                                    <p class = "pt-6 text-gray-600 font-bold uppercase text-sm">Location</p>
+                                    <p class = "pt-2 text-blue-400 pb-4 font-bold">{{contact.location}}</p>
 
+                                    <p class = "pt-6 text-gray-600 font-bold uppercase text-sm">Subject</p>
+                                    <p class = "pt-2 text-blue-400 pb-4 font-bold">{{contact.subject}}</p>
+                                </div>
+                                <div class="mb-2 pl-60">
+                                   
+                                    <b-avatar src="/img/Tcd Photo ID.jpeg" size="10rem"></b-avatar>
+                                </div>
+                               
+                             </div>
 
+                            <p class = "pt-4 text-gray-600 font-bold uppercase text-sm">Description</p>
+                            <p class = "pt-2 text-blue-400 pb-4 font-bold">{{contact.description}}</p>
+                        </div>
+                    </div>
+                </div>
+                  <!-- 
                     <div class = "flex items-center pt-6  pb-4">
                             <UserCircle :name = "contact.name"/>
                             <p class = "pl-5 text-xl font-bold ">{{contact.name}}</p>
@@ -54,8 +79,10 @@
                         <p class = "pt-2 text-blue-400 pb-4 font-bold">{{contact.description}}</p>
                     </div>
                 </div>
+                -->
                     
-            </div>
+            
+            
     </div>
 </template>
 
